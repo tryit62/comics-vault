e8f991a978a3d2cf28433bc66c41f5ccd69b8083
tryit62 <pinard.johan@icloud.com>
2026-08-20 15:24:46 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="comic-vault-v2.1-iconfix";const ASSETS=["./","index.html","style.css","app.js","manifest.webmanifest","apple-touch-icon-180.png","icon-192.png","icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
